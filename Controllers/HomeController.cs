a030564e48ae9052247e36c7202f5a4fb52fbec5
Adam Vít <vita24@spstrutnov.cz>
2026-02-23 14:33:36 +0100

    Přidána druhá stránka(Menu)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace Web_prace.Controllers
             return View();
         }
 
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
