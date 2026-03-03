b5e3e4fddccc60d3d21755e2fe8b7a9192f413f9
Adam Vít <vita24@spstrutnov.cz>
2026-03-03 10:33:56 +0100

    Vytvořené 3 nové prázdné stránky, přidán user.controller a  stránky jsou přidáné v menu

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_prace.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
